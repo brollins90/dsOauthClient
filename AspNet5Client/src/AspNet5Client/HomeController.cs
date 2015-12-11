b797d78c70364652aaed964e8ba4cb6601569e4c
brollins90 <brollins90@gmail.com>
2015-12-11 11:05:05 -0700

    added a aspnet5 client

---
+++ AspNet5Client/src/AspNet5Client/HomeController.cs
@@ -0,0 +1,47 @@
+﻿namespace AspNet5Client
+{
+    using Microsoft.AspNet.Authorization;
+    using Microsoft.AspNet.Mvc;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Threading.Tasks;
+
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        [Authorize]
+        public IActionResult Index(string error = "")
+        {
+            ViewBag.error = error;
+            return View();
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> Signout()
+        {
+            //await HttpContext.Authentication.SignOutAsync("DSAUTH");
+            await HttpContext.Authentication.SignOutAsync("Cookies");
+            return RedirectToAction("index");
+        }
+
+        public async Task<IActionResult> CallApi()
+        {
+            var token = User.FindFirst("access_token").Value;
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            try
+            {
+                var response = await client.GetStringAsync("http://localhost:8080/api/user");
+
+                ViewBag.Json = response.ToString();// JArray.Parse(response).ToString();
+            }
+            catch
+            {
+                return RedirectToAction("index", new { error = "error in api call" });
+            }
+            return View();
+        }
+    }
+}
---
+++ AspNet5Client/src/AspNet5Client/Startup.cs
@@ -0,0 +1,60 @@
+﻿namespace AspNet5Client
+{
+    using Microsoft.AspNet.Authentication.Cookies;
+    using Microsoft.AspNet.Builder;
+    using Microsoft.AspNet.Hosting;
+    using Microsoft.AspNet.Http;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using System.IdentityModel.Tokens.Jwt;
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(options => options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme);
+            services.AddMvc();
+            services.AddLogging();
+        }
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddDebug(LogLevel.Debug);
+            loggerFactory.AddConsole(LogLevel.Debug);
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            app.UseIISPlatformHandler();
+
+            app.UseCookieAuthentication(options =>
+            {
+                options.AutomaticAuthenticate = true;
+                options.AutomaticChallenge = true;
+                options.AuthenticationScheme = "Cookies";
+            });
+
+            app.UseOAuthAuthentication(options =>
+            {
+                options.AutomaticChallenge = true;
+                options.AuthenticationScheme = "DSAUTH";
+                options.DisplayName = "DSAUTH";
+                options.SignInScheme = "Cookies";
+
+                options.ClientId = "clientid1";
+                options.ClientSecret = "xoxoxo";
+                options.CallbackPath = new PathString("/login/oauthcallback");
+
+                //options.AuthorizationEndpoint = "http://ds.transvec.com/oauth/authorize";
+                //options.TokenEndpoint = "http://ds.transvec.com/oauth/token";
+                options.AuthorizationEndpoint = "http://localhost:8080/oauth/authorize";
+                options.TokenEndpoint = "http://localhost:8080/oauth/token";
+                options.Scope.Add("email");
+            });
+
+            app.UseDeveloperExceptionPage();
+            app.UseMvcWithDefaultRoute();
+        }
+
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
